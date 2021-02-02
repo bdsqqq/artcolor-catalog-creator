65c798e514b6c22c1e08956fe111250b09b36faa
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-01 21:53:58 -0300

    feat: added dummy prods data

---
+++ dummy/prods.ts
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: "AG0001",
+    img: "/images/background.jpg",
+    name: "Item 1",
+    desc: "desc",
+  },
+  {
+    id: "AG0002",
+    img: "/images/background.jpg",
+    name: "Item 2",
+    desc: "desc",
+  },
+  {
+    id: "AG0003",
+    img: "/images/background.jpg",
+    name: "Item 3",
+    desc: "desc",
+  },
+  {
+    id: "AG0004",
+    img: "/images/background.jpg",
+    name: "Item 4",
+    desc: "desc",
+  },
+  {
+    id: "AG0005",
+    img: "/images/background.jpg",
+    name: "Item 5",
+    desc: "desc",
+  },
+  {
+    id: "AG0006",
+    img: "/images/background.jpg",
+    name: "Item 6",
+    desc: "desc",
+  },
+  {
+    id: "AG0007",
+    img: "/images/background.jpg",
+    name: "Item 7",
+    desc: "desc",
+  },
+  {
+    id: "AG0008",
+    img: "/images/background.jpg",
+    name: "Item 8",
+    desc: "desc",
+  },
+  {
+    id: "AG0009",
+    img: "/images/background.jpg",
+    name: "Item 9",
+    desc: "desc",
+  },
+  {
+    id: "AG0010",
+    img: "/images/background.jpg",
+    name: "Item 7",
+    desc: "desc",
+  },
+  {
+    id: "AG0011",
+    img: "/images/background.jpg",
+    name: "Item 8",
+    desc: "desc",
+  },
+  {
+    id: "AG0012",
+    img: "/images/background.jpg",
+    name: "Item 9",
+    desc: "desc",
+  },
+];
